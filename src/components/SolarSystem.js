9c1431ee35c1dadb48047bb0f16ddd0aa1f64cb2
gustavomorais <gustavomorais.dev@gmail.com>
2023-03-06 04:44:33 -0300

    completa o requisito 6 que cria uma lista com os planetas

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetsData = planets.map((planet, index) => (
+      <li key={ index }>
+        <PlanetCard planetName={ planet.name } planetImage={ planet.image } />
+      </li>
+    ));
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planetsData }
       </div>
     );
   }
